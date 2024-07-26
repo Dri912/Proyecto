bc2f0db26b37f3102cdaa6cdf1ce972c26f026b0
Dri912 <166250220+Dri912@users.noreply.github.com>
2024-07-26 13:45:54 -0900

    inicio

---
+++ ProcesamientoDeImágenes.py
@@ -0,0 +1,8 @@
+import streamlit as st  
+
+# Título de la página
+st.title("Bienvenido a esta página para procesar imágenes.🖼️")
+
+# Breve descripción
+st.write("En esta página podrás escoger una imagen de tu galeria de fotos y "
+         "modificarla a tu gusto con las opciones que tenemos preparadas para ti.")
