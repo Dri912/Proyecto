b6c027435c460a87e18d32513c1c64e6fa2c7dae
Dri912 <166250220+Dri912@users.noreply.github.com>
2024-07-26 20:25:36 -0900

    tercero

--- ProcesamientoDeImágenes.py
+++ ProcesamientoDeImágenes.py
@@ -6,3 +6,16 @@ st.title("Bienvenido a esta página para procesar imágenes.🖼️")
 # Breve descripción
 st.write("En esta página podrás escoger una imagen de tu galeria de fotos y "
          "modificarla a tu gusto con las opciones que tenemos preparadas para ti.")
+
+opcion = st.radio("¿Listo?", ("Nada", "Sí", "No"))
+if opcion == "Sí":
+    nombre = st.text_input("¿Cómo te llamas?")
+
+    if nombre:
+        st.write(f"¡Buenas {nombre}😉!, escoge la imagen que deseas modificar:")
+
+        # Subir imagen  
+        ima = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
+        
+elif opcion == "No":
+    st.text("Lamentamos no poder ayudarte.😪")
